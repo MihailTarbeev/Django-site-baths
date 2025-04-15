31dd767984fc62902b1c073d1e6981160e5a73c9
Михаил_Тарбеев <tarb3@yandex.ru>
2025-04-15 23:05:38 +0300

    sixth commit

--- sitehuman/sitehuman/settings.py
+++ sitehuman/sitehuman/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2xqy4r03yjn)4e7a8%ar()op^8(bb+qvuj$w#@%hry&02#j%&7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
@@ -118,6 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [BASE_DIR / 'sitehuman/static',
+                    BASE_DIR / 'static',]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
