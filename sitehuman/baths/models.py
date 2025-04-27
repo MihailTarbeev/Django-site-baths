b0d8d7b996c7be7aa1fa3418c73f8fed7345568e
Михаил_Тарбеев <tarb3@yandex.ru>
2025-04-27 17:16:14 +0300

    ninth commit

--- sitehuman/baths/models.py
+++ sitehuman/baths/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Bath(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True)
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+    time_open = models.TimeField()
+    time_close = models.TimeField()
+    is_opened = models.BooleanField(default=True)
